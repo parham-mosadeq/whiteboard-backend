fd4d8a9276e8b059db581ffbd6fe1ab3308d3105
= <pmosadeqzadeh@gmail.com>
2025-07-17 22:32:20 +0330

    chore: created services to post and get

--- src/canvas/canvas.module.ts
+++ src/canvas/canvas.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CanvasController } from './canvas.controller';
 import { CanvasService } from './canvas.service';
+import { Canvas } from 'src/entities';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({ controllers: [CanvasController], providers: [CanvasService] })
+@Module({
+  controllers: [CanvasController],
+  providers: [CanvasService],
+  imports: [TypeOrmModule.forFeature([Canvas])],
+})
 export class CanvasModule {}
--- src/canvas/canvas.service.ts
+++ src/canvas/canvas.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { GetCanvasDto } from './dto';
+import { Canvas } from 'src/entities';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CanvasService {
-  getRoom({ roomId }: GetCanvasDto) {
-    return `here is your room :${roomId}`;
+  constructor(
+    @InjectRepository(Canvas)
+    private readonly canvasRepository: Repository<Canvas>,
+  ) {}
+
+  async getRoom({ roomId }: GetCanvasDto) {
+    const room = await this.canvasRepository.findOneBy({ roomId });
+    if (!room) {
+      throw new NotFoundException(`Room ${roomId} not found`);
+    }
+    return room;
   }
-  postRoom({ roomId }: GetCanvasDto) {
-    return `here is your room CREATED :${roomId}`;
+  async postRoom({ roomId }: GetCanvasDto): Promise<Canvas> {
+    const newRoom = this.canvasRepository.create({
+      roomId,
+      state: {},
+    });
+
+    return await this.canvasRepository.save(newRoom);
   }
 }
